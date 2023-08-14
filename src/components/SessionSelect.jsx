0e9d1dd927285538cd5627310b4895633467ebc5
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2023-08-14 22:41:58 +0200

    added conditionnal display for Session Select Screen

--- src/components/SessionSelect.jsx
+++ src/components/SessionSelect.jsx
@@ -13,6 +13,7 @@ function SessionSelect() {
     getRandomInArray(allMuscleGroup),
   ]);
   const [isSavedMuscleGroup, setIsSavedMuscleGroup] = useState(false);
+  const [firstTimeSaved, setFirstTimeSaved] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -30,6 +31,11 @@ function SessionSelect() {
   };
 
   const handleSaveMuscleGroup = (val) => {
+    if (!isSavedMuscleGroup) {
+      // the "rest" of StyledSessionSelect is hidden only the first time isSavedMuscleGroup turns from false to true.
+      // After that it remains always displayed, just refreshing the loaded data (exercices and eauipment)
+      setFirstTimeSaved(true);
+    }
     setIsSavedMuscleGroup(val);
   };
 
@@ -44,40 +50,43 @@ function SessionSelect() {
           isSavedMuscleGroup={isSavedMuscleGroup}
           handleSaveMuscleGroup={handleSaveMuscleGroup}
         />
-
-        {/* Changes only the number of reps/series (if displayed in the exercises list. For example : no numbers for AMRAP) and session duration */}
-        <h2>Session Type</h2>
-        <Selector>
-          <Button>{"<"}</Button>
-          <p>AMRAP</p>
-          <Button>{">"}</Button>
-        </Selector>
-        {/*Opens a modal*/}
-        {/*search by categories*/}
-        {/*Inputs for number of reps, default 10*/}
-        {/*Add Recommended number of exercises : 1-5 */}
-        <h2>Choose your Exercises</h2>
-        <Selector>
-          List
-          <Button onClick={handleOpen}>Modify</Button>
-        </Selector>
-        {/* depends on selected exercises, comes from loaded exercises data*/}
-        <h2>Necessary Equipment List (based on the exercises list)</h2>
-        <Selector>
-          <p>None (just bodyweight)</p>
-        </Selector>
-        {/* 5 per five, min 5 min*/}
-        <h2>Duration</h2>
-        <Selector>
-          <Button>-</Button>
-          <p>20 min</p>
-          <Button>+</Button>
-        </Selector>
-        <Button>GO ! </Button>
-        {open && (
-          <Modal>
-            <ExercicesModalContent handleClose={handleClose} />
-          </Modal>
+        {(firstTimeSaved || isSavedMuscleGroup) && (
+          <>
+            {/* Changes only the number of reps/series (if displayed in the exercises list. For example : no numbers for AMRAP) and session duration */}
+            <h2>Session Type</h2>
+            <Selector>
+              <Button>{"<"}</Button>
+              <p>AMRAP</p>
+              <Button>{">"}</Button>
+            </Selector>
+            {/*Opens a modal*/}
+            {/*search by categories*/}
+            {/*Inputs for number of reps, default 10*/}
+            {/*Add Recommended number of exercises : 1-5 */}
+            <h2>Choose your Exercises</h2>
+            <Selector>
+              List
+              <Button onClick={handleOpen}>Modify</Button>
+            </Selector>
+            {/* depends on selected exercises, comes from loaded exercises data*/}
+            <h2>Necessary Equipment List (based on the exercises list)</h2>
+            <Selector>
+              <p>None (just bodyweight)</p>
+            </Selector>
+            {/* 5 per five, min 5 min*/}
+            <h2>Duration</h2>
+            <Selector>
+              <Button>-</Button>
+              <p>20 min</p>
+              <Button>+</Button>
+            </Selector>
+            <Button>GO ! </Button>
+            {open && (
+              <Modal>
+                <ExercicesModalContent handleClose={handleClose} />
+              </Modal>
+            )}
+          </>
         )}
       </StyledSessionSelect>
     </>
