f4df7811364be07e44d7dc1a9e1756dbd0ac19be
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2023-08-29 15:40:40 +0200

    edited modal styling to enable scrollable content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,15 +14,15 @@ export default Modal;
 const StyledModal = styled.div`
   width: 80%;
   max-width: 500px;
-  height: 80%;
   background: white;
   margin: auto;
-  overflow: hidden;
 `;
 
 const Backdrop = styled.div`
   display: flex;
   position: fixed;
+  overflow: auto;
+  padding: 20px 0;
   top: 0;
   left: 0;
   width: 100vw;
